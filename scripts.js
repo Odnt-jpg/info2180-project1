c451b25358152f852cec2449e49ded3dfd087e94
Odnt <remyalexd2003@gmail.com>
2023-11-05 19:10:47 -0500

    Added Script and Made Edits to Stylesheet

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function () {
+
+    var textbox = document.querySelector('.newsletter form');
+    var etext = document.getElementById('email');
+    var popuptext = document.querySelector('.newsletter .message')
+
+    textbox.addEventListener('submit', function(event){
+
+            event.preventDefault();
+            
+            textcon = etext.value.trim(); 
+
+
+            if (textcon !== ''){
+
+                popuptext.textContent = 'Thank you! Your email address ' + textcon + ' has been added to our mailing list!';
+            } else {
+                popuptext.textContent = 'Please enter a valid email address.';
+            }
+
+            etext.value = '';
+    })
+
+
+
+});
